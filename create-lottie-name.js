9271e23df7abd828baa9ddc7143fbfc1a14076c3
luciyamaji <49602927+luciyamaji@users.noreply.github.com>
2023-10-05 13:21:13 +0100

    Update create-lottie-name.js

--- create-lottie-name.js
+++ create-lottie-name.js
@@ -1,69 +1,49 @@
-	var lottie, animations
-  
-	// Return a promise that resolves to true once animation is loaded
-	async function animationLoaded(animation) {
-		if(animation.isLoaded) {
-			return true
-		}
+var lottie, animations
 
-		return new Promise((resolve, reject) => {
-			animation.addEventListener('DOMLoaded', () => {
-				resolve(true)
+// Return a promise that resolves to true once animation is loaded
+async function animationLoaded(animation) {
+	if(animation.isLoaded) {
+		return true
+	}
+
+	return new Promise((resolve, reject) => {
+		animation.addEventListener('DOMLoaded', () => {
+			resolve(true)
+		})
+	})
+}
+
+// Return a promise that resolves to true once all animations are loaded
+async function waitForAnimationsLoaded(animations) {
+	await Promise.all(animations.map(animationLoaded))
+}
+
+async function initAnimations() {
+	lottie = Webflow.require('lottie').lottie
+	animations = lottie.getRegisteredAnimations()
+	animations.forEach( item => {
+		item.name = item.wrapper.getAttribute('data-w-id')
+		// item.wrapper.removeAttribute('data-src')
+	})
+	await waitForAnimationsLoaded(animations)
+}
+
+var Webflow = Webflow || []
+
+Webflow.push(() => {
+	initAnimations()
+		.then(() => {
+			console.log('Initialized animations')
+			console.log(animations)
+			
+			// Reset animations to 0
+			lottie.goToAndStop(0, true)
+			
 			})
 		})
-	}
+		.catch((error) => {
+			console.error(error)
+		})
+})
 
-	// Return a promise that resolves to true once all animations are loaded
-	async function waitForAnimationsLoaded(animations) {
-		await Promise.all(animations.map(animationLoaded))
-	}
 
-	async function initAnimations() {
-		lottie = Webflow.require('lottie').lottie
-		animations = lottie.getRegisteredAnimations()
-    animations.forEach( item => {
-    	item.name = item.wrapper.getAttribute('data-w-id')
-      // item.wrapper.removeAttribute('data-src')
-    })
-		await waitForAnimationsLoaded(animations)
-	}
-
-	var Webflow = Webflow || []
-
-	Webflow.push(() => {
-		initAnimations()
-			.then(() => {
-				console.log('Initialized animations')
-				// hide splash screen
-        // Reset animations to 0
-        console.log(animations)
-        lottie.goToAndStop(0, true)
-        
-        // Play preloader
-        const preLoader = document.querySelector('[data-lottie-interaction="pre-loader"]').getAttribute("data-w-id")
-        lottie.play(preLoader)
-        setTimeout(function () {
-        	lottie.goToAndStop(0, true, preLoader)
-        }, 3000)
-        
-        // Get buttons with interactions
-        var btn = document.querySelectorAll('[data-lottie-interaction="hover"]')
-
-        btn.forEach(item => {
-					// Get the name of the lottie
-          var lottie_name = item.querySelector('[data-animation-type="lottie"]').getAttribute("data-w-id")
-          // Hover functions
-          item.addEventListener('mouseenter', function () {
-            lottie.play(lottie_name)
-          })
-          item.addEventListener('mouseleave', function () {
-            lottie.goToAndStop(0, true, lottie_name)
-          })
-        })
-			})
-			.catch((error) => {
-				console.error(error)
-			})
-	})
-  
-  
