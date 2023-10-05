e7d1525dd47606cacb4c831122e16b2720a5fb69
luciyamaji <49602927+luciyamaji@users.noreply.github.com>
2023-10-05 15:24:24 +0100

    Update color-picker.js

--- color-picker.js
+++ color-picker.js
@@ -20,22 +20,18 @@ var colorPicker = new iro.ColorPicker('#picker', {
 
 
 const editButton = document.querySelectorAll("#editButton");
-const closeButton = document.querySelectorAll("#editClose");
-
-// console.log(editButton);
 
 editButton.forEach(item => {
   item.addEventListener("click", function(e) {
     // Get parent with button and edit popup
     var currentItem = e.currentTarget.parentNode.parentNode.parentNode;
+//      console.log(currentItem);
     
     // Get current lottie and play
     var lottie_edit = currentItem.querySelector("#lottie-edit");
     var lottie_name = lottie_edit.getAttribute("data-w-id");
     var lottie_src = lottie_edit.getAttribute("data-src");
     lottie.play(lottie_name);
-    console.log(lottie_src);
-    console.log(typeof lottie_src);
     
     // Get current picker
     var hexInput = currentItem.querySelector("#hexInput");
@@ -61,15 +57,38 @@ editButton.forEach(item => {
         paths[i].style.stroke = this.hexString;
       };
     });
+    
+    // Get current download button
+    var downloadEditedBtn = currentItem.querySelector("#downloadEdited");
+    
+    downloadEditedBtn.addEventListener('click', function () {
+      // Get current lottie JSON
+      fetch(lottie_src) 
+          .then((res) => { 
+          return res.json(); 
+      }) 
+      .then((data) => {
+          var lottieColor = data.layers[0].ef[0].ef[0].v.k;
+          lottieColor[0] = colorPicker.color.red / 255;
+          lottieColor[1] = colorPicker.color.green / 255;
+          lottieColor[2] = colorPicker.color.blue / 255;
+          console.log(lottieColor);
+          console.log(data);
+    
+          const str = 'data:text/json;charset=utf-8,' + JSON.stringify(data);
+          const newData = encodeURI(str);
 
-    import data from lottie_src assert { type: 'json' };
-    console.log(data);
-  
-  });
-});
-
-closeButton.forEach(item => {
-  item.addEventListener("click", function(e) {
-    lottie.goToAndStop(0, true);
+          const element = document.createElement("a");
+          element.setAttribute('href', newData);
+          element.setAttribute('download', 'data.json');
+          element.click();
+      });
+    });
+    
+    // Get close button
+    var closeButton = currentItem.querySelector("#editClose");
+    closeButton.addEventListener('click', function() {
+      lottie.goToAndStop(0, true, lottie_name);
+    });
   });
 });
